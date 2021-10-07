8813519eb465d6bfe5d132552c976c50aac753df
Thiago Prazeres <thiagoprazeres@gmail.com>
2021-10-07 04:23:21 -0300

    home route

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-
-
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [],
+  imports: [CommonModule],
 })
-export class HomeModule { }
+export class HomeModule {}
