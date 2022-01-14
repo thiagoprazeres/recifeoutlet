b3b9c938de106a15ca7f6ac48c36ca5caca92556
Thiago Prazeres <thiagoprazeres@gmail.com>
2022-01-14 04:52:55 -0300

    reinstalação do Google Analytics

--- scully.recifeoutlet.config.ts
+++ scully.recifeoutlet.config.ts
@@ -1,23 +1,10 @@
-import { setPluginConfig, ScullyConfig, prod } from '@scullyio/scully';
-import { GoogleAnalytics } from '@scullyio/scully-plugin-google-analytics';
-
-const defaultPostRenderers = [];
-
-if (prod) {
-  setPluginConfig(GoogleAnalytics, { globalSiteTag: 'G-1VSWDYK2DK' });
-  defaultPostRenderers.push(GoogleAnalytics);
-}
+import { ScullyConfig } from '@scullyio/scully';
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'recifeoutlet',
   outDir: './dist/static',
-  defaultPostRenderers,
   routes: {
-    '/': {
-      type: 'contentFolder',
-      postRenderers: [...defaultPostRenderers],
-    },
     '/noticias/:title': {
       type: 'contentFolder',
       title: {
